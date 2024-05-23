e03a414c58d9531a6b3ce41d9934a3bb395514fa
TANISH SHARMA <101451522+tanishs874@users.noreply.github.com>
2024-05-23 18:25:31 +0000

    difference between update and alter command

---
+++ difference_between_alter_update.sql
@@ -0,0 +1,31 @@
+--> Alter --> Alter is a Data definition language
+--> Alter will work on the structure of data
+
+--> Update --> Update is a Data Manipulation language
+--> Update will work on the data
+
+--> emp table
+id | Name | Salary |
+1  | A    | 1000   |
+2  | B    | 2000   |
+3  | C    | 3000   |
+
+--> Lets say we want to update the salary.So, we will use update command for that
+
+update emp set salary = salary * 2;
+
+--> emp table
+id | Name | Salary |
+1  | A    | 2000   |
+2  | B    | 4000   |
+3  | C    | 6000   |
+
+--> Let's say we want to increase the salary of person A only 
+
+update emp set salary = salary * 2 where id = 1;
+
+--> emp table
+id | Name | Salary |
+1  | A    | 4000   |
+2  | B    | 4000   |
+3  | C    | 6000   |
